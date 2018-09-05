870669c2a55d8b395c28c57f9b55d75aaf868565
liufoqi <1139329576@qq.com>
2018-09-05 09:28:49 +0800

    9.5

---
+++ app.js
@@ -0,0 +1,10 @@
+//app.js
+const Towxml=require('./towxml/main');
+App({
+  onLaunch: function () {
+  },
+  towxml:new Towxml(),
+  globalData: {
+    userInfo: null
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,39 @@
+//index.js
+//获取应用实例
+import {fetch} from'../../utils/util.js'
+const app = getApp()
+Page({
+ data:{
+   swiperData: [],
+   mainContent:[],
+   indicatorDots: true,
+   autoplay: false,
+   interval: 3000,
+   duration: 500,
+ },
+ onLoad(){
+    this.getData()
+    this.getContent()
+ },
+ getData(){
+  fetch.get('/swiper').then(res=>{
+    this.setData({
+      swiperData:res.data
+    })
+  })
+ },
+ getContent(){
+   fetch.get('/category/books').then(res=>{
+     this.setData({
+       mainContent:res.data
+     })
+   })
+ },
+ jumpBook(event){
+  const id=event.currentTarget.id
+  console.log(id)
+  wx.navigateTo({
+    url: `/pages/details/details?id=${id}`
+  })
+ }
+})
